dd7aad863ea8b131657e92ce6c1ac934d7e89019
Jeremiah Barton <boateng@byu.edu>
2025-10-07 18:14:27 -0600

    debugged navbar.jsx file

--- demoVite/src/components/navbar.jsx
+++ demoVite/src/components/navbar.jsx
@@ -1,27 +1,27 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <nav> style={styles.nav}
+    <nav style={styles.nav}>
       <Link style={styles.link} to="/">Home</Link>
-      <Link stlye={styles.link} to="/play">Play</Link>\
+      <Link style={styles.link} to="/play">Play</Link>
       <Link style={styles.link} to="/scores">Scores</Link>
-      <Link style={styles.link} to="about">About</Link>
+      <Link style={styles.link} to="/about">About</Link>
     </nav>
   );
 }
 const styles = {
-    nav: {
-        backgroundColor: "#f4f4f4",
-        padding: "10px",
-        display: "flex",
-        justifyContent: "center",
-        gap: "20px",
-        borderBottom: "2px solid #ccc"
-    },
-    link: {
-        textDecoration: "none",
-        color: "#333",
-        fontWeight: "bold"
-    }
+  nav: {
+    backgroundColor: "#f4f4f4",
+    padding: "10px",
+    display: "flex",
+    justifyContent: "center",
+    gap: "20px",
+    borderBottom: "2px solid #ccc",
+  },
+  link: {
+    textDecoration: "none",
+    color: "#333",
+    fontWeight: "bold",
+  },
 };
