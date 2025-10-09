ecdf1fc5822c1bb82c5231bb79520744aa9cf61c
Jeremiah Barton <boateng@byu.edu>
2025-10-08 19:29:11 -0600

    updated home.jsx to reflect my code in index.html

--- demoVite/src/components/home.jsx
+++ demoVite/src/components/home.jsx
@@ -1,8 +1,28 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to Pie Vote!</h1>
-      <p>This is the home page.</p>
-    </div>
+    <main className="container text-center">
+      <h1 className="mb-3">Welcome to the Pie Vote App</h1>
+      <p className="lead mb-4">
+        Vote on your Favorite Pie Flavor, see the most poular flavors, and chat with other pie fans!
+      </p>
+      <form method="get" action="/play" className="mx-auto" style={{maxWidth: "400ox"}}>
+        <div className="mb-3 input-group">
+          <span className="input-group-text">@</span>
+          <input type="text" className="form-control" placeholder="your@email.com"/>
+        </div>
+        <div className="mb-3 input-group">
+          <span className="input-group-text">🔒</span>
+          <input type="password" className="form-control" placeholder="password"/>
+        </div>
+        <div className="d-flex justify-content-between">
+          <button type="submit" className="btn btn-primary">Login</button>
+          <button type="submit" className="btn btn-secondary">Create</button>
+        </div>
+      </form>
+      <footer className="text-center mt-5 py-3 border-top">
+        <span>Created by Jeremiah Barton</span><br />
+        <a href="https://github.com/jeremiahu2/startup"></a>
+      </footer>
+    </main>
   );
 }
