a10e3fa87e84c83dea295e276e59a5e67d3147fd
Jeremiah Barton <boateng@byu.edu>
2025-11-04 11:54:23 -0700

    Added a placeholder API to index.js, and created vite.config.js

--- service/index.js
+++ service/index.js
@@ -1,11 +1,20 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const bcrypt = require('bcryptjs');
-const uuid = require('uuid');
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import bcrypt from 'bcryptjs';
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('public'));
 
-const port = process.argv.length > 2 ? process.argv[2] : 4000;
+// Placeholder Endpoint
+app.get('/api/hello', (req, res) => {
+  res.send({ message: 'Hello from your startup backend!' });
+});
+
+app.listen(port, () => {
+  console.log(`Startup service listening on port ${port}`);
+});
