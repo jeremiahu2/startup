e1aa43c1d94a08dd77426513d38c6bee03bcefaf
Jeremiah Barton <boateng@byu.edu>
2025-11-12 23:50:48 -0700

    updated db.js withthe correct pathing

--- service/db.js
+++ service/db.js
@@ -1,7 +1,13 @@
 import { MongoClient } from 'mongodb';
 import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const config = JSON.parse(fs.readFileSync('dbConfig.json', 'utf8'));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const configPath = path.join(__dirname, 'dbConfig.json');
+const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+
 const uri = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
@@ -10,7 +16,7 @@ let db;
 export async function connectToDB() {
   await client.connect();
   db = client.db('260Domain');
-  console.log("Connected to MongoDB");
+  console.log("✅ Connected to MongoDB");
 }
 
 export function getDB() {
